4be6c3dc0c044937bd2dc793e762e77ca64ab2f5
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-06-27 10:35:04 +0300

    Back-end: Added dto

---
+++ Advertising application/back-end/src/main/java/lt/techin/exam/advertising_app/dto/AdvertDto.java
@@ -0,0 +1,18 @@
+package lt.techin.exam.advertising_app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdvertDto {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String comment;
+}
