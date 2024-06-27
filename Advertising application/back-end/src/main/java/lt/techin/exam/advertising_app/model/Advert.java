2dad89e1cd0d26339f931c9527c35a63a738f359
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-06-27 10:27:57 +0300

    Back-end Added entities

---
+++ Advertising application/back-end/src/main/java/lt/techin/exam/advertising_app/model/Advert.java
@@ -0,0 +1,30 @@
+package lt.techin.exam.advertising_app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "adverts")
+public class Advert {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column
+    private String comment;
+
+}
---
+++ Advertising application/back-end/src/main/java/lt/techin/exam/advertising_app/model/Category.java
@@ -0,0 +1,23 @@
+package lt.techin.exam.advertising_app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+}
