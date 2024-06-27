cec2766939e426c602db744b4e79c890a97a79ee
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-06-27 09:40:47 +0300

    Added back-end starter pack

---
+++ Advertising application/back-end/src/main/java/lt/techin/exam/advertising_app/AdvertisingAppApplication.java
@@ -0,0 +1,13 @@
+package lt.techin.exam.advertising_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertisingAppApplication.class, args);
+	}
+
+}
---
+++ Advertising application/back-end/src/test/java/lt/techin/exam/advertising_app/AdvertisingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.exam.advertising_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
