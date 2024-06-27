552d4be98da92daab81e0109d6f93d3c7b5248ac
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-06-27 10:06:57 +0300

    Added Front-end

---
+++ Advertising application/front-end/src/HelloWorld.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const HelloWorld = () => {
+  return <div className="text-center">HelloWorld</div>;
+};
+
+export default HelloWorld;
